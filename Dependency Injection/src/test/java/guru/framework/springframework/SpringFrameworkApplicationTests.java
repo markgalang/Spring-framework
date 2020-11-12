d612b621f200006c7b5cbf4b1f3f84d2183c164e
Mark Galang <galangmarkg@gmail.com>
2020-11-12 16:42:35 -0500

    Initiate Project Structure

---
+++ Dependency Injection/src/test/java/guru/framework/springframework/SpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.framework.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
